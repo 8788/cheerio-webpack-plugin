d0bfff16a50fb025f2eb9d3327864f5039045d3c
8788 <2802513+8788@users.noreply.github.com>
2018-12-23 22:18:05 +0800

    add 0.1.0

---
+++ index.js
@@ -0,0 +1,39 @@
+const cheerio = require('cheerio')
+
+function matchFile (filename, test) {
+  let match = false
+  if (Array.isArray(test)) {
+    match = test.some(item => item.test(filename))
+  } else if (Object.prototype.toString.call(test) == '[object RegExp]') {
+    match = test.test(filename)
+  } else {
+    console.error('test must be a RegExp or array of RegExps')
+  }
+  return match
+}
+
+class CheerioWebpackPlugin {
+  constructor (options) {
+    this.options = options
+  }
+
+  apply (compiler) {
+    compiler.hooks.emit.tapAsync('CheerioWebpackPlugin', (compilation, callback) => {
+      for (const filename in compilation.assets) {
+        if (matchFile(filename, this.options.test)) {
+          const $ = cheerio.load(compilation.assets[filename].source())
+          this.options.callback && this.options.callback($)
+
+          compilation.assets[filename] = {
+            source: () => $.html(),
+            size: () => $.html().size
+          }
+        }
+      }
+
+      callback()
+    })
+  }
+}
+
+module.exports = CheerioWebpackPlugin
